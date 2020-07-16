60a27a36ce7d93bf7798043b67e5adda6340953f
maduiep <emmanuelmaduip@gmail.com>
2020-07-16 21:59:48 +0100

    modified previous files and added icon folder

--- todoList/script.js
+++ todoList/script.js
@@ -1,25 +1,28 @@
 const todoForm = document.querySelector('.todo-form')
-const todoInput = document.querySelelctor('#todo-input')
-const todoList = document.querySelelctor('.todo-list')
+const todoInput = document.querySelector('#todo-input')
+const todoList = document.querySelector('.todo-list')
+const theButton = document.querySelector('#theButton')
 
-let listItems = []
+var listItems = {};
 
-let localStorage = localStorage.getItem('todoItem');
 
-todoForm.addEventListener('submit', function (e) {
- e.preventDefault()
- if (validate(todoInput.value)) {
+theButton.addEventListener('click', function (e) {
+ e.preventDefault();
+
+ if (!validate(todoInput.value)) {
   todoInput.style.borderColor = 'red';
   return;
  }
 
- todoInput.style.borderColor = 'none';
+ todoInput.style.borderColor = 'lightGreen';
 
+ let theItems = manageTodo();
 
+ manageDisplay(theItems);
 
 })
 
-function addTodo(item) {
+function validate(item) {
  if (item === '') {
   return false;
  }
@@ -27,7 +30,30 @@ function addTodo(item) {
 }
 
 function manageTodo() {
- if (typeof localStorage === 'undefined') {
-  listItems.push(todoInput.value);
+
+ if (localStorage.getItem('todoItem') !== null) {
+  listItems = JSON.parse(localStorage.getItem('todoItem'));
+  console.log(listItems)
  }
+
+ listItems[Object.keys(listItems).length] = todoInput.value;
+
+
+ localStorage.setItem('todoItem', JSON.stringify(listItems));
+
+ return listItems;
+
+}
+
+function manageDisplay(items) {
+
+ //loop through the value from db and diplay 
+ let text = '';
+ console.log(items)
+ for (var i in items) {
+  text += `<li>${items[i]}</li>`;
+ }
+
+ document.querySelector('#todo-list').innerHTML = text;
+
 }
